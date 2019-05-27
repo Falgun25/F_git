ea350e6c497619043b089a8da55d1725cfb31770
Falgun <falgun.dudhat@rapidops.com>
2019-05-27 12:50:01 +0530

    message

---
+++ ex3.sh
@@ -0,0 +1,16 @@
+#bin/bash
+
+a=$1
+
+b=$2
+
+if [ $a<$b ]
+then
+	echo "$a is max"
+	echo "$b is min"
+else
+	echo "$a is min"
+        echo "$b is max"
+
+if [ $a AND $b]
+
---
+++ ex7.sh
@@ -0,0 +1,5 @@
+#bin/bash
+
+cmd=$(cp -r $1 $2)
+
+echo "$cmd"
